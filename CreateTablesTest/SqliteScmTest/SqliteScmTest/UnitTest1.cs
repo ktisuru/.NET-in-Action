401b271fe6f660d80f58e440d839c62274a05479
Isuru Kaluthotage <isuru.k@itx360.com>
2022-05-22 23:43:34 +0530

    fourth commit

--- CreateTablesTest/SqliteScmTest/SqliteScmTest/UnitTest1.cs
+++ CreateTablesTest/SqliteScmTest/SqliteScmTest/UnitTest1.cs
@@ -6,8 +6,8 @@ namespace SqliteScmTest
 {   //this is comment
     public class UnitTest1 : IClassFixture<SampleScmDataFixture>
     {
-      private SampleScmDataFixture fixture;
-      private ScmContext context;
+        private SampleScmDataFixture fixture;
+        private ScmContext context;
 
 
         public UnitTest1(SampleScmDataFixture fixture)
@@ -30,6 +30,30 @@ namespace SqliteScmTest
             Assert.Equal(part.Id, item.PartTypeId);
             Assert.Equal(100, item.Count);
             Assert.Equal(10, item.OrderThreshold);
+        }
+
+        [Fact]
+
+        public void TestPartCommands()
+        {
+            var item = context.Inventory.First();
+            var startCount = item.Count;
+            context.CreatePartCommand(new PartCommand()
+            {
+                PartTypeId = item.PartTypeId,
+                PartCount = 10,
+                Command = PartCountOperation.Add
+            });
+
+            context.CreatePartCommand(new PartCommand()
+            {
+                PartTypeId = item.PartTypeId,
+                PartCount = 5,
+                Command = PartCountOperation.Remove
+            });
+            var inventory = new Inventory(context);
+            inventory.UpdateInventory();
+            Assert.Equal(startCount + 5, item.Count);
         }
     }
 }
--- CreateTablesTest/WidgetScmDataAccess/WidgetScmDataAccess/InventoryItem.cs
+++ CreateTablesTest/WidgetScmDataAccess/WidgetScmDataAccess/InventoryItem.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace WidgetScmDataAccess
 {
     public class InventoryItem
     {
         public int PartTypeId { get; set; }
+        public PartType Part { get; set; }
         public int Count { get; set; }
         public int OrderThreshold { get; set; }
     }
